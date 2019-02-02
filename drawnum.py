30070257a98ff2d946512ec0de6758411372d4f2
marcantoine <marc-antoine.guery@etu.univ-lyon1.fr>
2019-02-02 19:07:57 +0100

    Drawing tool -Cleaned code -Added size control and exception handling

--- drawnum.py
+++ drawnum.py
@@ -4,55 +4,80 @@ import numpy as np
 import time
 import math
 
-ranx = int(100)
-rany = int(100)
-a = np.zeros((ranx,rany))
-a[0][0]=1
-fig,ax = plt.subplots()
-heat = plt.imshow(a, cmap='gray')
-
-def update_data(event, mat):
-	xmou = int(event.xdata)
-	ymou = int(event.ydata)
-	if mat[ymou][xmou]>0.8:
-		return mat
-	interx=int(ranx/25)
-	intery=int(rany/25)
-	maxx=min(xmou+interx, ranx)
-	minx=max(xmou-interx, 0)
-	maxy=min(ymou+intery, rany)
-	miny=max(ymou-intery, 0)
-	for yare in range(miny, maxy):
-		normy = (maxy-ymou) - math.fabs(yare-ymou)
-		valy = normy/(maxy-ymou)
+class Numer(object):
+	"""
+	Matplotlib heatmap in which a number can be drown.
+	"""
+	def __init__(self, x=100, y=100, size=3):
+		"""
+		Initialize the object with number of x cells, y cells and size of the line.
+		"""
+		try:
+			assert size in range(1, 5)
+			assert x in range(10, 500)
+			assert y in range(10, 500)
+		except AssertionError:
+			print("x and y must be between 10 and 500. Input is x : %s, y : %s." % (x, y))
+			print("Size must be between 1 and 5. Input is %s." % (size))
+			raise
+		self.ranx=x
+		self.rany=y
+		self.size=size
+		self.coords=np.zeros((self.ranx, self.rany))
+		self.coords[0][0]=1
+		self.fig, self.ax = plt.subplots()
+		self.heatmap = plt.imshow(self.coords, cmap='gray')
+		self.fig.tight_layout()
+		plt.axis('off')
+	
+	def reset_coords(self):
+		self.coords=np.zeros((self.ranx, self.rany))
+		self.coords[0][0]=1
+	
+	def update_data(self, event):
+		xmou = int(event.ydata)
+		ymou = int(event.xdata)
+		if self.coords[xmou][ymou]>0.8:
+			return
+		interx=int(self.ranx // (1/self.size*100) + 1)
+		intery=int(self.rany // (1/self.size*100) + 1)
+		maxx=min(xmou+interx-1, self.ranx)
+		minx=max(xmou-interx+1, 0)
+		maxy=min(ymou+intery-1, self.rany)
+		miny=max(ymou-intery+1, 0)
 		for xare in range(minx, maxx):
 			normx = (maxx-xmou) - math.fabs(xare-xmou)
 			valx = normx/(maxx-xmou)
-			mat[yare][xare]=min(mat[yare][xare]+(valx+valy)/2, 1)
-	return mat
-def hover(event):
-	mat = a
-	if event.inaxes == ax:
-		cont = heat.contains(event)
-		if cont:
-			newmat = update_data(event, mat)
-			heat.set_data(newmat)
-			fig.canvas.draw_idle()
+			for yare in range(miny, maxy):
+				normy = (maxy-ymou) - math.fabs(yare-ymou)
+				valy = normy/(maxy-ymou)
+				self.coords[xare][yare]=min(self.coords[xare][yare]+(valx+valy)/3, 1)
 
-def key(event):
-	cid = fig.canvas.mpl_connect("motion_notify_event", hover)
-	if event.key!='control':
-		fig.canvas.mpl_disconnect(cid)
+	def hover(self, event):
+		if event.inaxes == self.ax:
+			cont = self.heatmap.contains(event)
+			if cont:
+				self.update_data(event)
+				self.heatmap.set_data(self.coords)
+				self.fig.canvas.draw_idle()
 
-def button(event):
-	global a #pas bien
-	a = np.zeros((ranx,rany))
-	a[0][0]=1
-	heat.set_data(a)
-	fig.canvas.draw_idle()
+	def key(self, event):
+		cid = self.fig.canvas.mpl_connect("motion_notify_event", self.hover)
+		if event.key!='control':
+			self.fig.canvas.mpl_disconnect(cid)
 
-fig.canvas.mpl_connect("key_press_event", key)
-fig.canvas.mpl_connect('button_press_event', button)
+	def button(self, event):
+		self.reset_coords()
+		self.heatmap.set_data(self.coords)
+		self.fig.canvas.draw_idle()	
+	
+	def draw(self):
+		"""
+		Activate the drawing process
+		"""
+		self.fig.canvas.mpl_connect("key_press_event", self.key)
+		self.fig.canvas.mpl_connect('button_press_event', self.button)
+		plt.show()
 
-
-plt.show()
+drawtest=Numer(x=100, y=100, size=3)
+drawtest.draw()
